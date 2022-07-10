37aefb3b06c01ba14b06ec78917d986b4e475e6e
dihan48 <dihan4894@mail.ru>
2022-07-10 18:41:14 +0300

    port

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-'use strict';
+"use strict";
 
-const bodyParser = require('body-parser');
-const browserify = require('browserify-middleware');
-const express = require('express');
-const { readdirSync, statSync } = require('fs');
-const { join } = require('path');
+const bodyParser = require("body-parser");
+const browserify = require("browserify-middleware");
+const express = require("express");
+const { readdirSync, statSync } = require("fs");
+const { join } = require("path");
 
-const { mount } = require('./lib/server/rest/connectionsapi');
-const WebRtcConnectionManager = require('./lib/server/connections/webrtcconnectionmanager');
+const { mount } = require("./lib/server/rest/connectionsapi");
+const WebRtcConnectionManager = require("./lib/server/connections/webrtcconnectionmanager");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const examplesDirectory = join(__dirname, 'examples');
+const examplesDirectory = join(__dirname, "examples");
 
-const examples = readdirSync(examplesDirectory).filter(path =>
-  statSync(join(examplesDirectory, path)).isDirectory());
+const examples = readdirSync(examplesDirectory).filter((path) =>
+  statSync(join(examplesDirectory, path)).isDirectory()
+);
 
 function setupExample(example) {
   const path = join(examplesDirectory, example);
-  const clientPath = join(path, 'client.js');
-  const serverPath = join(path, 'server.js');
+  const clientPath = join(path, "client.js");
+  const serverPath = join(path, "server.js");
 
   app.use(`/${example}/index.js`, browserify(clientPath));
   app.get(`/${example}/index.html`, (req, res) => {
-    res.sendFile(join(__dirname, 'html', 'index.html'));
+    res.sendFile(join(__dirname, "html", "index.html"));
   });
 
   const options = require(serverPath);
@@ -35,18 +36,22 @@ function setupExample(example) {
   return connectionManager;
 }
 
-app.get('/', (req, res) => res.redirect(`${examples[0]}/index.html`));
+app.get("/", (req, res) => res.redirect(`${examples[0]}/index.html`));
 
 const connectionManagers = examples.reduce((connectionManagers, example) => {
   const connectionManager = setupExample(example);
   return connectionManagers.set(example, connectionManager);
 }, new Map());
 
-const server = app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
   const address = server.address();
   console.log(`http://localhost:${address.port}\n`);
 
-  server.once('close', () => {
-    connectionManagers.forEach(connectionManager => connectionManager.close());
+  server.once("close", () => {
+    connectionManagers.forEach((connectionManager) =>
+      connectionManager.close()
+    );
   });
 });
